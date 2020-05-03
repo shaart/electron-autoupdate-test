b89cf368fa1398c068fdbafcc1c3eab8ce1456e5
Artur Shalaev <imshaart@gmail.com>
2020-05-04 02:08:47 +0300

    Semicolons added

--- build/build-installer.js
+++ build/build-installer.js
@@ -1,16 +1,16 @@
-const shell = require('shelljs')
+const shell = require('shelljs');
 
-shell.echo('##########################')
-shell.echo('#    Building electron   #')
-shell.echo('##########################')
+shell.echo('##########################');
+shell.echo('#    Building electron   #');
+shell.echo('##########################');
 
 if (!shell.test('-e', 'spring/target')) {
-  shell.echo('Error: server is not built yet.')
+  shell.echo('Error: server is not built yet.');
   shell.exit(1)
 }
 
-shell.rm('-rf', 'dist')
+shell.rm('-rf', 'dist');
 if (shell.exec('electron-builder build').code !== 0) {
-  shell.echo('Error: electron build failed')
+  shell.echo('Error: electron build failed');
   shell.exit(1)
 }

818cc0e68a912904a7ed931baf05909e20e587f6
Artur Shalaev <imshaart@gmail.com>
2020-05-08 02:42:38 +0300

    Logs of updating and downloading added, version increased to 0.0.7

--- electron/preload.js
+++ electron/preload.js
@@ -17,13 +17,15 @@ window.interop = {
     ipcRenderer.on('update_available', () => {
       ipcRenderer.removeAllListeners('update_available');
       message.innerText = 'A new update is available. Downloading now...';
+      logger.info('A new update is available. Downloading now...');
       notification.classList.remove('hidden');
     });
 
     ipcRenderer.on('update_downloaded', () => {
       ipcRenderer.removeAllListeners('update_downloaded');
-      message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
+      logger.info('Update Downloaded. It will be installed on restart');
       restartButton.classList.remove('hidden');
+      message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
       notification.classList.remove('hidden');
     });
   },

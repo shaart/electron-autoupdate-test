72407d5c6b3756f6d1a75f9b924e9209dc997c71
Artur Shalaev <imshaart@gmail.com>
2020-05-08 02:10:03 +0300

    Git repo, author, deploy command added

--- build/build-installer.js
+++ build/build-installer.js
@@ -10,7 +10,16 @@ if (!shell.test('-e', 'spring/target')) {
 }
 
 shell.rm('-rf', 'dist');
-if (shell.exec('electron-builder build').code !== 0) {
-  shell.echo('Error: electron build failed');
-  shell.exit(1)
+
+const FIRST_ARGUMENT_INDEX = 2;
+if (process.argv[FIRST_ARGUMENT_INDEX] === 'deploy') {
+  if (shell.exec('electron-builder build --win --publish always').code !== 0) {
+    shell.echo('Error: electron build and deploy failed');
+    shell.exit(1)
+  }
+} else {
+  if (shell.exec('electron-builder build --win --publish never').code !== 0) {
+    shell.echo('Error: electron build failed');
+    shell.exit(1)
+  }
 }
